cfd7bb56cc0d3458d83afdf89c4c64294d4cc470
Baptiste Augrain <daiyam@zokugun.org>
2026-08-17 11:44:30 +0200

    fix: correctly lookup for EditorConfig section

--- src/utils/getEditorConfigSection.ts
+++ src/utils/getEditorConfigSection.ts
@@ -1,9 +1,13 @@
 import { EditorConfigOptions } from '@zokugun/eslint-toolkit';
 import { IniResult } from '@zokugun/ini-parse-lite';
 
+const WORD_REGEX = /^\w+$/
+
 export function getEditorConfigSection(pattern: string, config: IniResult): EditorConfigOptions | undefined {
+	const lookup = WORD_REGEX.test(pattern) ? `*.${pattern}` : pattern;
+
 	for(const [key, section] of Object.entries(config)) {
-		if(key === pattern || key.includes(`{${pattern}`) || key.includes(`,${pattern}`)) {
+		if(key === lookup || key.includes(`{${lookup}`) || key.includes(`,${lookup}`)) {
 			return section;
 		}
 	}
